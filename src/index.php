5417f68106922212d864730d921abd10197736fc
Elena <kencrimea@gmail.com>
2019-07-02 02:20:11 +0300

    start template

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>РЕГИДРОН БИО</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+<header class="header"></header>
+
+<main class="wrapper content" role="main"></main>
+
+</body>
+</html>
